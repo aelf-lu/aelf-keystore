2ccf3d20b8e505f7289360d1523fcd80d480bcef
aelf-lu <yimeng.lu@aelf.io>
2023-12-05 15:08:40 +0800

    fix: fix encrypt method

--- src/AElfKeyStoreService.cs
+++ src/AElfKeyStoreService.cs
@@ -11,9 +11,6 @@ public class AElfKeyStoreService : KeyStoreService
     public string EncryptKeyStoreAsJson(string password, string privateKey)
     {
         var keyPair = CryptoHelper.FromPrivateKey(ByteArrayHelper.HexStringToByteArray(privateKey));
-        if (keyPair?.PrivateKey == null || keyPair.PublicKey == null)
-            throw new InvalidKeyPairException("Invalid keypair (null reference).", null);
-
         var address = Address.FromPublicKey(keyPair.PublicKey);
         return EncryptAndGenerateDefaultKeyStoreAsJson(password, keyPair.PrivateKey, address.ToBase58());
     }
